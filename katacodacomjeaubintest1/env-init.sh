cbf043b32a8a045d559639b11b95ecb0ecedea56
jeaubin <jeaubin@cisco.com>
2019-01-11 20:06:32 -0500

    Add apt update for ubuntu

--- katacodacomjeaubintest1/env-init.sh
+++ katacodacomjeaubintest1/env-init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+apt-get update
 apt-get install -y python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install pyats
 pip3 install genie
-pip3 install tabulate
+#pip3 install tabulate
